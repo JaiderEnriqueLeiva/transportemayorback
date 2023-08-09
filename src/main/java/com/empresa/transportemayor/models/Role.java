c514ed65bbe5ec6a6ea35d40717cd78421161070
JaiderEnriqueLeiva <jaiderleiva15@gmail.com>
2023-08-09 17:51:12 -0400

    h2 configurations

---
+++ src/main/java/com/empresa/transportemayor/models/Role.java
@@ -0,0 +1,23 @@
+package com.empresa.transportemayor.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+}
