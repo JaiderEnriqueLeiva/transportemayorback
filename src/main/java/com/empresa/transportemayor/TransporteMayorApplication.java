ea5a8b1ac5496f28e70dced5d28933de3243f8c9
JaiderEnriqueLeiva <jaiderleiva15@gmail.com>
2023-08-09 16:05:19 -0400

    init commit

---
+++ src/main/java/com/empresa/transportemayor/TransporteMayorApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.transportemayor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporteMayorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransporteMayorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresa/transportemayor/TransporteMayorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.transportemayor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransporteMayorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
