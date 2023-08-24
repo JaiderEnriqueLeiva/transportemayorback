2973724e878d356dda78ee1d005e216121356e9d
Jaider Enrique Leiva <jaiderleiva15@gmail.com>
2023-08-24 18:15:25 -0400

    cors vulnerability checked

--- src/main/java/com/empresa/transportemayor/controller/VehicleController.java
+++ src/main/java/com/empresa/transportemayor/controller/VehicleController.java
@@ -7,18 +7,14 @@ import java.util.List;
 import javax.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/vehicle")
+@CrossOrigin(origins = "*")
 public class VehicleController {
 
   /* todo vehiculos:
